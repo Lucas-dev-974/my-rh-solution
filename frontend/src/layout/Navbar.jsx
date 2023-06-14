67eaa8be71a46de087a6b4c1290028708b3fe51a
lucas <lucas.lvn97439@gmail.com>
2023-06-14 19:11:39 +0400

    add navbar

---
+++ frontend/src/layout/Navbar.jsx
@@ -0,0 +1,9 @@
+import { A } from "@solidjs/router"; 
+
+export default function () {
+    return <nav class="flex h-[80px] items-center justify-center bg-[var(--gray-dark)] text-white">
+        <A href="/" class="text-2xl">
+            MY RH SOLUTION
+        </A>
+    </nav>
+}
