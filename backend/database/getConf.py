ad072b268c9b3b4e2a80f0da2fee564fa4972c45
lucas <lucas.lvn97439@gmail.com>
2023-06-14 18:40:23 +0400

    update getConfig

--- backend/database/getConf.py
+++ backend/database/getConf.py
@@ -1,8 +1,6 @@
-import configparser
+from dotenv import dotenv_values
 
 # Permet de garder les identifiants de connexion de la BDD dans l'environnement
 def getConfig(name):
-    config = configparser.ConfigParser()
-    config.read('.venv/pyvenv.cfg')       
-    
-    return config.get('venv', name)
+    vars = dotenv_values('.env')    
+    return vars[name]
