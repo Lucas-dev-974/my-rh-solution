a8bf1d26ec8d6679f740b11f42ccdb59741f2827
lucas <lucas.lvn97439@gmail.com>
2023-06-14 19:40:48 +0400

    add "404 page not found" component

---
+++ frontend/src/views/404.jsx
@@ -0,0 +1,5 @@
+export default function () {
+    return <div class="flex items-center justify-center text-white text-4xl h-[91.3vh] bg-[var(--dark-gray)]">
+        <h1>404 - la page n'existe pas !</h1>
+    </div>
+} 
