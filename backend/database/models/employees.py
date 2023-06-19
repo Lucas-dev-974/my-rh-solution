1b76392b155877ddca566097fcaadce410703b97
lucas <lucas.lvn97439@gmail.com>
2023-06-19 23:49:52 +0400

    wip

--- backend/database/models/employees.py
+++ backend/database/models/employees.py
@@ -3,19 +3,18 @@ from sqlalchemy_serializer import SerializerMixin
 
 try:
     from database import Base
-except:
+except:  # noqa: E722
     from ..database import Base
-    
+
+
 class Employees(Base, SerializerMixin):
-    __tablename__ = 'employees'
+    __tablename__ = "employees"
 
-    id     = Column(Integer, primary_key=True, nullable = False) 
+    id = Column(Integer, primary_key=True, nullable=False)
 
-    email  = Column(String)
+    email = Column(String)
     gender = Column(String)
     first_name = Column(String)
-    last_name  = Column(String)
+    last_name = Column(String)
     phone_number = Column(String)
     email_personnal = Column(String)
-
-    
