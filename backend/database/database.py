9fa7105d2e573df0dda1290f521de502bbe026f7
lucas <lucas.lvn97439@gmail.com>
2023-06-19 23:50:58 +0400

    wip

--- backend/database/database.py
+++ backend/database/database.py
@@ -3,27 +3,28 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-try:
-    from getConf import getConfig
-except:
-    from .getConf import getConfig
+# try:
+#     from getConf import getConfig
+# except:
+#     from .getConf import getConfig
 
 
 connection_url = os.environ.get("CONNECTION_URL")
-if connection_url is None:
-    connection_url = (
-        "postgresql://"
-        + getConfig("USER")
-        + ":"
-        + getConfig("PASSWORD")
-        + "@localhost:5434/"
-        + getConfig("DATABASE")
-        + ""
-    )
+print(connection_url)
+# if connection_url is None:
+#     connection_url = (
+#         "postgresql://"
+#         + getConfig("USER")
+#         + ":"
+#         + getConfig("PASSWORD")
+#         + "@localhost:5434/"
+#         + getConfig("DATABASE")
+#         + ""
+#     )
 
 
-engine = create_engine(connection_url)
+# engine = create_engine(connection_url)
 
-session = sessionmaker(bind=engine)
-session = session()
-Base = declarative_base()
+# session = sessionmaker(bind=engine)
+# session = session()
+# Base = declarative_base()
