ab833f87a2b4e3fb4988939be8443613d7ba9cd2
lucas <lucas.lvn97439@gmail.com>
2023-06-19 23:48:52 +0400

    wip

--- backend/database/database.py
+++ backend/database/database.py
@@ -1,5 +1,5 @@
-
-from sqlalchemy     import create_engine
+import os
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -7,12 +7,23 @@ try:
     from getConf import getConfig
 except:
     from .getConf import getConfig
-    
 
-''' Create engine and session communicate with database '''
-engine  = create_engine('postgresql://' + getConfig('USER') + ':' + getConfig('PASSWORD')
-                        + '@localhost:5434/' + getConfig('DATABASE') + '', echo=True)
+
+connection_url = os.environ.get("CONNECTION_URL")
+if connection_url is None:
+    connection_url = (
+        "postgresql://"
+        + getConfig("USER")
+        + ":"
+        + getConfig("PASSWORD")
+        + "@localhost:5434/"
+        + getConfig("DATABASE")
+        + ""
+    )
+
+
+engine = create_engine(connection_url)
+
 session = sessionmaker(bind=engine)
 session = session()
-
 Base = declarative_base()
