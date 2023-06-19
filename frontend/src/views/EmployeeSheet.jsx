f52d1ed20c54b94cb6edb35f79f14b4376c46ba7
lucas <lucas.lvn97439@gmail.com>
2023-06-19 13:30:07 +0400

    code formated

--- frontend/src/views/EmployeeSheet.jsx
+++ frontend/src/views/EmployeeSheet.jsx
@@ -1,72 +1,116 @@
 import { createEffect, createSignal } from "solid-js";
 import request from "../services/request";
 
-
-
 export default function () {
-    const [employee, setEmployee] = createSignal({})
-    const id = location.href.substring(location.href.lastIndexOf('/') + 1)
+  const [employee, setEmployee] = createSignal({});
+  const id = location.href.substring(location.href.lastIndexOf("/") + 1);
 
-    let employer = {}
-    
-    createEffect( async () => {
-        const response = await request('employee/' + id, 'GET', {}) 
-        setEmployee(response)
-        employer = response
-    })
+  let employer = {};
 
-    const update = async () => {
-        const response = await request('employee/', 'PATCH', JSON.stringify(employee))
-    }
+  createEffect(async () => {
+    const response = await request("employee/" + id, "GET", {});
+    setEmployee(response);
+    employer = response;
+  });
 
-    return <div class="container">
-        <div className="flex justify-center">
-            FICHE EMPLOYEE
+  const update = async () => {
+    const response = await request(
+      "employee/",
+      "PUT",
+      JSON.stringify(employer)
+    );
+  };
+
+  return (
+    <div class="container">
+      <div className="flex justify-center">FICHE EMPLOYEE</div>
+
+      <section id="employee-sheet">
+        <div>
+          <label htmlFor="prenom">Prénom</label>
+          <input
+            id="prenom"
+            class="text-dark"
+            type="text"
+            value={employee().first_name}
+            onChange={(e) => {
+              employer.first_name = e.target.value;
+              update();
+            }}
+          />
         </div>
-        
-        <section id="employee-sheet">
-            <div>
-                <label htmlFor="prenom">Prénom</label>
-                <input id="prenom" class="text-dark" type="text" value={employee().first_name} onChange={(e) => {
-                    employer.last_name = e.target.value
-                    update()
-                }}/>
-            </div>
-            <div>
-                <label htmlFor="nom">Nom</label>
-                <input id="nom" class="text-dark" type="text" value={employee().last_name} onChange={(e) => {
-                    employer.name = e.target.value
-                    update()
-                }}/>
-            </div>
-            <div>
-                <label htmlFor="email">Email</label>
-                <input id="email" class="text-dark" type="text" value={employee().email} onChange={(e) => {
-                    employer.email = e.target.value
-                    update()
-                }}/>
-            </div>
-            <div>
-                <label htmlFor="email-personnel">Email personnel</label>
-                <input id="email-personnel" class="text-dark" type="text" value={employee().email_personnal} onChange={(e) => {
-                    employer.email_personnal = e.target.value
-                    update()
-                }}/>
-            </div>           
-            <div>
-                <label htmlFor="phone">Téléphone</label>
-                <input id="phone" class="text-dark" type="text" value={employee().phone_number} onChange={(e) => {
-                    employer.phone_number = e.target.value
-                    update()
-                }}/>
-            </div>
-            <div>
-                <label htmlFor="genre">Genre</label>
-                <select id="genre" class="text-dark">
-                    <option class="text-dark" value="male">Male</option>
-                    <option class="text-dark" value="female">Female</option>
-                </select>
-            </div>
-        </section>
+        <div>
+          <label htmlFor="nom">Nom</label>
+          <input
+            id="nom"
+            class="text-dark"
+            type="text"
+            value={employee().last_name}
+            onChange={(e) => {
+              employer.last_name = e.target.value;
+              update();
+            }}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            class="text-dark"
+            type="text"
+            value={employee().email}
+            onChange={(e) => {
+              employer.email = e.target.value;
+              update();
+            }}
+          />
+        </div>
+        <div>
+          <label htmlFor="email-personnel">Email personnel</label>
+          <input
+            id="email-personnel"
+            class="text-dark"
+            type="text"
+            value={employee().email_personnal}
+            onChange={(e) => {
+              employer.email_personnal = e.target.value;
+              update();
+            }}
+          />
+        </div>
+        <div>
+          <label htmlFor="phone">Téléphone</label>
+          <input
+            id="phone"
+            class="text-dark"
+            type="text"
+            value={employee().phone_number}
+            onChange={(e) => {
+              employer.phone_number = e.target.value;
+              update();
+            }}
+          />
+        </div>
+        <div>
+          <label htmlFor="genre">Genre</label>
+          <select
+            id="genre"
+            class="text-dark"
+            value={employee().gender}
+            onChange={(e) => {
+              employer.gender = e.target.value;
+              update();
+            }}
+          >
+            <option class="text-dark" value="Male">
+              Male
+            </option>
+            <option class="text-dark" value="Female">
+              Female
+            </option>
+          </select>
+        </div>
+      </section>
     </div>
+  );
 }
